9793db7f6d2a9fc18d693221877b8e6086e4d31f
codersclub <vvotintsev@gmail.com>
2014-07-04 16:23:16 +0400

    Fixed copyrights in language files

--- upload/source/language/fa/block/lang_doinglist.php
+++ upload/source/language/fa/block/lang_doinglist.php
@@ -4,7 +4,7 @@
  *      [Discuz!] (C)2001-2099 Comsenz Inc.
  *      This is NOT a freeware, use is subject to license terms
  *
- *      $Id: lang_doinglist.php by Valery Votintsev at sources.ru
+ *      $Id: lang_doinglist.php by Valery Votintsev, codersclub.org
  */
 
 if(!defined('IN_DISCUZ')) {
--- upload/source/language/fa/task/lang_connect_bind.php
+++ upload/source/language/fa/task/lang_connect_bind.php
@@ -4,7 +4,7 @@
  *      [Discuz!] (C)2001-2099 Comsenz Inc.
  *      This is NOT a freeware, use is subject to license terms
  *
- *      $Id: lang_connect_bind.php by Valery Votintsev at sources.ru
+ *      $Id: lang_connect_bind.php by Valery Votintsev, codersclub.org
  *	Translated to Persian by Discuz.ir
  */
 
